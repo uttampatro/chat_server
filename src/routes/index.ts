6b92582da642ac6351a9b163254c6ce42b8d3034
Uttam Patro <uttampatro890@gmail.com>
2021-06-05 18:03:45 +0530

    login

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,7 +9,7 @@ router.get('/', (_req: Request, res: Response) => {
 });
 
 // User controller
-router.get('/users', UserController.fetchUserProfile);
+router.get('/login', UserController.fetchUserProfile);
 router.post('/login', UserController.loginUser);
 
 // Chat controller
